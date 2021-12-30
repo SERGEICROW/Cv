25083fb805b220a2f4ad042e27b15eb0565769ff
Roy <rodyancar@gmail.com>
2021-12-30 12:00:43 -0600

    Apis margin top responsive fix

--- pages/apis.tsx
+++ pages/apis.tsx
@@ -4,7 +4,7 @@ import TwitterApp from "../components/twitterTrends";
 
 const Apis = () => (
 
-    <Container className="relative z-30 py-5 flex flex-col items-center justify-center
+    <Container className="relative z-30 pt-24 pb-10 flex flex-col items-center justify-center
     sm:border border-green-500 min-h-screen select-none">
         <TwitterApp/>
     </Container>
